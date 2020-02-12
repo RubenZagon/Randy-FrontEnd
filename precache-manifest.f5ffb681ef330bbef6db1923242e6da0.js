a25130c6d25fbf3f3d3fd49368fa642fb180e755
Ruben Zagon <ruben.zagon@gmail.com>
2020-02-12 03:28:36 +0000

    Updates

---
+++ precache-manifest.f5ffb681ef330bbef6db1923242e6da0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b11142bb36e5c0dad3ae8f2e60d579",
+    "url": "/Randy-FrontEnd/index.html"
+  },
+  {
+    "revision": "42f2b6efd785581d6f99",
+    "url": "/Randy-FrontEnd/static/css/main.407cc622.chunk.css"
+  },
+  {
+    "revision": "240b17ed8acf6d864ef2",
+    "url": "/Randy-FrontEnd/static/js/2.3b9979a8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Randy-FrontEnd/static/js/2.3b9979a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42f2b6efd785581d6f99",
+    "url": "/Randy-FrontEnd/static/js/main.7b6dadfd.chunk.js"
+  },
+  {
+    "revision": "b14a8dc4d9fcef675fa6",
+    "url": "/Randy-FrontEnd/static/js/runtime-main.42da3fbe.js"
+  }
+]);
